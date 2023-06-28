dbd1b77dcf0dc6d1c1539f0f703903770f2fc320
kostya <kotsazonov@gmail.com>
2023-06-28 15:33:07 +0300

    Home word

---
+++ task41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//  сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+ Console.WriteLine("Введите количество чисел :");
+        int m = int.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            Console.WriteLine($"Введите число {i + 1}:");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Количество чисел, больших нуля: {count}");
+
+    
+
+    
+
---
+++ task43/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+//   уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+ Console.WriteLine("Введите значения k1, b1, k2 и b2:");
+
+        // Ввод коэффициентов прямых
+        Console.Write("k1: ");
+        double k1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("b1: ");
+        double b1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("k2: ");
+        double k2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("b2: ");
+        double b2 = Convert.ToDouble(Console.ReadLine());
+
+        // Проверка на параллельность прямых
+        if (k1 == k2)
+        {
+            Console.WriteLine("Прямые параллельны. Точки пересечения не существует.");
+        }
+        else
+        {
+            // Вычисление координат точки пересечения
+            double x = (b2 - b1) / (k1 - k2);
+            double y = k1 * x + b1;
+
+            Console.WriteLine($"Точка пересечения: ({x}, {y})");
+        }
+
+        Console.ReadLine();
+    
+
